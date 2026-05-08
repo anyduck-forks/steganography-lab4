a2562f62076d02e05ea8da12af7e2fb4f95172c7
Dmytro Anyduck <dmytro.anyduck@gmail.com>
2026-05-08 11:59:39 +0300

    fix: use correct fallback

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	},
 	kit: {
 		adapter: adapter({
-			fallback: '200.html'
+			fallback: 'index.html'
 		}),
 		paths: {
             base: process.env.NODE_ENV === 'production' ? '/steganography-lab4' : '',
